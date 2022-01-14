95dd9cac85cf99b019e6fa886ffc80c9af8e0201
yingyue cao <yingyue3@ualberta.ca>
2022-01-13 17:48:10 -0700

    new trial

---
+++ trial.py
@@ -0,0 +1,22 @@
+from pymongo import MongoClient
+
+
+client = MongoClient('mongodb://localhost:27017')
+db = client['Test']
+
+
+name = db.customers
+
+
+results = artists.aggregate([
+    
+    {"$project": {"_id":1, "artist_id":1, "name":1, "num_tracks":{"$size":"$tracks"}}
+        
+    },
+    {"$match": {"num_tracks": {"$gt":0}}}
+    
+])
+
+
+for i in results:
+    print(i)
